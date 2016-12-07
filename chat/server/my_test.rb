755c275a17000f42e22966d56a25cfaeb538c92e
aviklar10 <aviklar10@gmail.com>
2016-12-08 00:27:05 +0200

    remove unnecessary files from git.

--- chat/server/my_test.rb
+++
@@ -1,9 +0,0 @@
-require 'mysql2'
-
-con = Mysql2::Client.new(:host => 'localhost', :username => 'admin', :password => '1234', :database => 'chatdb')
-rs = con.query('select * from message_containers')
-rs.each do |row|
-  puts row
-end
-
-con.close
